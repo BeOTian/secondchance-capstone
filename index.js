1296a9a6a1bfba58337dd3c71d9d51d40fe66a16
BeOTian <145973402+BeOTian@users.noreply.github.com>
2026-08-16 20:16:20 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+const natural = require('natural'); // Dòng này AI quét
+const app = require('./app');
+const port = 3060;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
